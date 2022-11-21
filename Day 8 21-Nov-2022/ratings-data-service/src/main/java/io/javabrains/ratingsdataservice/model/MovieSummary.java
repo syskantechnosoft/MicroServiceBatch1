b158ec2b94b5ad048991c1f29c343582b9ffdcc2
Siva OS <syskantechnosoft@users.noreply.github.com>
2022-11-21 12:29:27 +0530

    Add files via upload

---
+++ Day 8 21-Nov-2022/ratings-data-service/src/main/java/io/javabrains/ratingsdataservice/model/MovieSummary.java
@@ -0,0 +1,51 @@
+package io.javabrains.ratingsdataservice.model;
+
+public class MovieSummary {
+
+	private String id;
+	private String title;
+	private int runtime;
+	private int vote_count;
+	private double vote_average;
+
+	public double getVote_average() {
+		return vote_average;
+	}
+
+	public void setVote_average(double vote_average) {
+		this.vote_average = vote_average;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getRuntime() {
+		return runtime;
+	}
+
+	public void setRuntime(int runtime) {
+		this.runtime = runtime;
+	}
+
+	public int getVote_count() {
+		return vote_count;
+	}
+
+	public void setVote_count(int vote_count) {
+		this.vote_count = vote_count;
+	}
+
+}
